df0e62d22f8d6a151514ad1fa2a975724fb14030
Robbie <Robbie@email.com>
2023-06-29 11:12:58 +0900

    init commit

---
+++ src/main/java/com/example/sparta/test0629/Test0629Application.java
@@ -0,0 +1,13 @@
+package com.example.sparta.test0629;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0629Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test0629Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sparta/test0629/Test0629ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparta.test0629;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test0629ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
